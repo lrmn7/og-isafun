44a0aaed7da0c521d073b69538a408c9064f5f7f
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-12 18:17:05 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   const [title, setTitle] = useState<string>("Title");
   const [desc, setDesc] = useState<string>("Description");
   const [imgUrl, setImgUrl] = useState<string>("");
-  const [siteName, setSiteName] = useState<string>("yehezgun.com");
+  const [siteName, setSiteName] = useState<string>("is-a.fun");
   const [isCopied, setIsCopied] = useState<boolean>(false);
   const ogUrl = new URL(
     `/api/og?title=${title}&desc=${desc}${
