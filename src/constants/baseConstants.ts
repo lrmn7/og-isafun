87dfd8d5bb7d9d5df44973fa6bc3cb2a952d314b
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-12 18:04:27 +0700

    Update baseConstants.ts

--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -1,3 +1,3 @@
-export const baseURL = process.env.BASE_URL ?? "http://og.is-a.fun/";
+export const baseURL = process.env.BASE_URL ?? "http://og.lrmn.site/";
 export const baseImageURL =
-  "https://res.cloudinary.com/dswardhd9/image/upload/v1683883016/Open_Peeps_-_Bust_1_sz6cvh.png";
+  "https://res.cloudinary.com/dswardhd9/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1683883016/Open_Peeps_-_Bust_1_sz6cvh.png";
